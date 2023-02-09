3a4be4ffb961d9f21d9707cba02427040fcdea4e
Alice <alicewengs@gmail.com>
2023-02-09 08:53:57 -0800

    installed mongoose to backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log(`Connected to database: ${process.env.MONGO_URI}`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
