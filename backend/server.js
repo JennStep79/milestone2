35ccc35b42ecbf322b84f1bb33d692f21380d95f
Alice <alicewengs@gmail.com>
2023-02-11 14:02:37 -0800

    changed config.js to have process.env username, password, name, host

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,8 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
-    console.log(`Connected to database: ${process.env.MONGO_URI}`);
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.use("/api/entertainment", require("./controllers/entertainment_controller"));
 app.use("/api/foods", require("./controllers/foods_controller"));
@@ -17,6 +15,10 @@ app.use("/api/misc", require("./controllers/misc_controller"));
 app.use("/api/relationships", require("./controllers/relationships_controller"));
 app.use("/api/thrills", require("./controllers/thrills_controller"));
 app.use("/api/travels", require("./controllers/travels_controller"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
