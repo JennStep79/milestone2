84dea471616ef2b7207e447155549f89ce11bc9e
Alice <alicewengs@gmail.com>
2023-02-13 18:11:53 -0800

    compiled category routes into one file

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../build")));
 
 app.use("/api/categories", require("./controllers/categories_controller"));
-app.use("/api/entertainment", require("./controllers/entertainment_controller"));
-app.use("/api/foods", require("./controllers/foods_controller"));
-app.use("/api/misc", require("./controllers/misc_controller"));
-app.use("/api/relationships", require("./controllers/relationships_controller"));
-app.use("/api/thrills", require("./controllers/thrills_controller"));
-app.use("/api/travels", require("./controllers/travels_controller"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../build/index.html"));
