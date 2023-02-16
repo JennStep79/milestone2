42777d94d218229ac73f0e357eaa193dd0c3a78f
Alice <alicewengs@gmail.com>
2023-02-16 13:56:39 -0800

    added associations between models

--- backend/models/list.js
+++ backend/models/list.js
@@ -9,9 +9,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Comment}) {
-      // define association here
-      List.hasMany(Comment, {foreignKey:"comment_id"});
+    static associate({Category, Comment}) {
+      List.hasOne(Comment, {
+        foreignKey: "comment_id"
+      });
+      List.belongsTo(Category, {
+        foreignKey: "category_id"
+      });
     }
   }
   List.init({
