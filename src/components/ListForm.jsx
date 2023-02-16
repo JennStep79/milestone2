224a48ed07bcadc8682926468d208396d5e17128
Diana-Cano <dianacanoo@icloud.com>
2023-02-15 22:08:24 -0800

    added functioning  fetch request to list form

--- src/components/ListForm.jsx
+++ src/components/ListForm.jsx
@@ -1,17 +1,31 @@
 import {useState} from 'react'
 
 function ListForm(){
- const [idea, setIdea] = useState({});
-    // function handleChange(e){
-    //     setIdea(e.target.value)
-    // }
-    const handleChange = e=>setIdea(e.target.value)
+ const [idea, setIdea] = useState({list_item:""});
+    async function handleSubmit(e){
+        e.preventDefault()
+        try{
+            const response = await fetch('http://localhost:3001/api/lists',{
+                method:"POST",
+                headers:{
+                    "Content-Type": "application/json;charset-utf-8"
+                },
+                body: JSON.stringify(idea)
+            });
+            // if(!response.ok) {
+            //     throw new error ("network response was not ok")
+            // }
+            return await response.json();
+        }catch (error){
+            console.error("there was a problem with the fetch operation:",error)
+        }
+    }
+    const handleChange = e=>setIdea({list_item:e.target.value})
 
     return(
         <div>
-            <form onSubmit={e=>{
-                e.preventDefault()}}>
-                <label for='idea'>Insert awesome idea here;</label>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='idea'>Insert awesome idea here;</label>
                 <textarea id='idea' maxLength={255} onChange={handleChange}></textarea>
                 <button type = 'submit'>Submit</button>
 
