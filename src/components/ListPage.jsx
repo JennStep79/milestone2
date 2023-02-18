9dd70af6e50f7d7f48ae5f8122d80f3f6e2649f2
Alice <alicewengs@gmail.com>
2023-02-17 23:07:23 -0800

    updated list page to have update and delete button

--- src/components/ListPage.jsx
+++ src/components/ListPage.jsx
@@ -1,7 +1,25 @@
-function ListPage() {
+import { useState } from "react";
+
+function ListPage(props) {
+    const [list, updateList] = useState({
+        title: "",
+        list_item: ""
+    });
+
+    const handleDelete = () => {
+        
+    };
+
+    const handleUpdate = () => {
+
+    };
+
     return (
         <div>
-            <h1>whats up</h1>
+            <h1>{props.list.title}</h1>
+            <p>{props.list.list_item}</p>
+            <button onClick={handleUpdate}>Update</button>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     )
 }
