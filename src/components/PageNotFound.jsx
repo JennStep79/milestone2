38b23faa9f97e2cf01bacc7605fd771f228244a0
Alice <alicewengs@gmail.com>
2023-02-20 13:50:33 -0800

    test

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,6 +2,7 @@ function PageNotFound() {
     return (
         <div>
             <img className="error" src={require("../img/404.png")} alt="A rain cloud over an empty bucket and the words 'Oh no! This bucket is empty! 404"/>
+            <img className="home" src={require("../img/404-home.png")} alt="A rainbow that says 'take me home'."/>
         </div>
     )
 }
