59db60306e620a58806495dca83721054d7ebe51
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-02-20 18:55:10 -0800

    update img path

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -24,7 +24,7 @@ function Categories() {
         <>
             <div className="container">
                 <img className="logo"
-                src={require("../img/logowithtagline.png")}
+                src={"../img/logowithtagline.png"}
                 alt="Buck-It's logo, a bucket with a rainbow coming out of it saying 'fill your bucket before you kick it'."/>
             </div>
             <div className="container">
