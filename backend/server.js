a1488fec8fd11b4660be2c7c18d98d85fa6a32ab
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-02-20 19:10:41 -0800

    change get route

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use("/api/categories", require("./controllers/categories_controller"));
 app.use("/api/lists", require("./controllers/lists_controller"));
 app.use("/api/comments", require("./controllers/comments_controller"));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
